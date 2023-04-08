c30a243832a0661c3c48a0912f8c7d1039c02865
Gunadi PS <gndsulistiawan@gmail.com>
2023-04-08 10:38:15 +0700

    menambahkan cors

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const express = require("express")
 const app = express()
 app.use(express.urlencoded({extended:false}))
 
+app.use((request, response, next)=> {
+    // response.setHeader("Access-Control-Allow-Origin", "https://cdpn.io")
+    response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500")
+    // response.setHeader("Access-Control-Allow-Origin", "*")
+    response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+    response.setHeader("Access-Control-Allow-Headers", "Content-type")
+    next()
+})
+
 app.use("/", require("./src/routes"))
 
 const PORT = process.env.PORT
