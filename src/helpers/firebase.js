ebcc1068df885e9dc22c4cd4f314029a16f50688
Gunadi PS <gndsulistiawan@gmail.com>
2023-07-14 08:52:13 +0700

    fix: firebase json env

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -1,6 +1,8 @@
 var admin = require("firebase-admin")
 
-var serviceAccount = require("../../pushnotif-eventapp-firebase-adminsdk-jl2vm-582bd74ba8.json")
+// var serviceAccount = require("../../pushnotif-eventapp-firebase-adminsdk-jl2vm-582bd74ba8.json")
+
+var serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS)
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
